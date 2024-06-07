ab5d1382ef68a461bb569658b8e2da01e573e7d4
RidhamSharma781 <ridhamsharma781@gmail.com>
2024-06-07 21:10:31 +0530

    Added Education

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import './Education.css'
+import { education } from '../../Data/constants'
 
 const Education = () => {
   return (
     <div className='education'>
-        <h1>Education</h1>
-        <p>My education has been a journey of self-discovery and growth. My educational details are as follows.</p>
-      
+      <h1>Education</h1>
+      <p>My education has been a journey of self-discovery and growth. My educational details are as follows.</p>
+      <section className="container-big-education">
+        <div className="border-helper">
+        {education.map((stage, idx) => {
+          return <div key={idx} className="education-container">
+            <div className="header">
+              <img src={stage.img} alt="" />
+              <div className="education-info">
+                <div className="name">{stage.school}</div>
+                <div className="class">{stage.degree}</div>
+                <div className="year">{stage.date}</div>
+              </div>
+            </div>
+            <div className="grade">
+              <b>Grade :</b> <span>{stage.grade}</span>
+            </div>
+            <p>{stage.desc}</p>
+          </div>
+        })}
+        </div>
+        
+      </section>
     </div>
   )
 }
