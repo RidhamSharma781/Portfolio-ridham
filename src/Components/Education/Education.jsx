a4116be5bbc4cda74f902136f7cffc7513419e0d
RidhamSharma781 <ridhamsharma781@gmail.com>
2024-06-12 23:50:06 +0530

    Started resposive at 568px

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -7,7 +7,7 @@ const Education = () => {
     <div className='education' id='education'>
       <h1>Education</h1>
       <p>My education has been a journey of self-discovery and growth. My educational details are as follows.</p>
-      <section className="container-big-education">
+      <div className="container-big-education">
     
         {education.map((stage, idx) => {
           return <div key={idx} className="education-container">
@@ -27,7 +27,7 @@ const Education = () => {
         })}
       
         
-      </section>
+      </div>
     </div>
   )
 }
